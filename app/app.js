a5637c913f0198c006873d87772ea16d6609a599
azebra70 <thahn70u@gmail.com>
2022-06-20 17:35:58 +0900

    fetch적용2

--- app/app.js
+++ app/app.js
@@ -17,6 +17,7 @@
 
 // 모듈
 const express = require('express');
+const bodyParser = require("body-parser");
 const app = express();
 
 
@@ -27,6 +28,8 @@ const home = require("./src/routes/home");
 app.set("views","./src/views");
 app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/src/public`));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
 
 app.use("/", home);   // use --> 미들웨어를 등록해주는 메소드
 
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,15 +1,25 @@
 "use strict";    // javascript 사용시 적용
 
-const home = (req,res) => {
-    res.render("home/index");
+const output = {
+    home: (req,res) => {
+        res.render("home/index");
+    },
+    
+    login: (req,res) => {
+        res.render("home/login");
+    },
 };
 
-const login = (req,res) => {
-    res.render("home/login");
+const process = {
+    login: (req,res) => {
+        console.log(req.body);
+    },
 };
+
+
 
 // Object에서 key:value를 key만 정의할 경우 key:key와 동일하게 처리
 module.exports = {
-    home,
-    login,
+    output,
+    process,
 };

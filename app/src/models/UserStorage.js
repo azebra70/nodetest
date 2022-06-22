0722fa18a1e89e8e7341160222c883c126c31934
thahn <thahn70@gmail.com>
2022-06-22 23:19:39 +0900

    fs를 통한 회원등록

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,4 +1,7 @@
 "use strict";
+
+const { isatty } = require("tty");
+
 const fs = require("fs").promises;
 
 // const { use } = require("../routes/home");
@@ -17,8 +20,9 @@ class UserStorage {
         return userInfo;  
     }
 
-    static getUsers(...fields){
-        // const users = this.#users; 
+    static #getUsers(data,isAll,fields) {
+        const users = JSON.parse(data);
+        if (isAll) return users;
         const newUsers = fields.reduce((newUsers,field) => {
             if (users.hasOwnProperty(field)) {
                 newUsers[field] = users[field];
@@ -26,6 +30,17 @@ class UserStorage {
             return newUsers;
         },{});
         return newUsers;
+
+    }
+
+    static getUsers(isAll, ...fields){
+        return fs
+        .readFile("./src/databases/users.json")
+        .then((data) => {
+          return this.#getUsers(data,isAll,fields);
+        })
+        .catch(console.error);
+
     }
 
     static getUserInfo(id){
@@ -37,16 +52,16 @@ class UserStorage {
           .catch(console.error);
     }
 
-    static save(userInfo) {
-        // const users = this.#users;
-        
+    static async save(userInfo) {
+        const users = await this.getUsers(true);
+        if ( users.id.includes(userInfo.id) ) {
+            throw "이미 존재하는 아이디 입니다.";
+        }
         users.id.push(userInfo.id);
-        users.name.push(userInfo.name);
         users.psword.push(userInfo.psword);
-
-        return {success: true};
-        // console.log(users);
-
+        users.name.push(userInfo.name);
+        fs.writeFile("./src/databases/users.json", JSON.stringify(users));
+        return { success: true };
     }
 }
 
